9c0c51cbf48897b79375fde0b88b473b0ba6f31f
Luca Modica <luca.modica@edu.unito.it>
2021-10-29 14:43:30 +0100

    Partial MVC es

---
+++ 06_GUI_ObserverObservable/ProverbsApp/src/com/mvc/ProverbsModel.java
@@ -0,0 +1,38 @@
+package com.mvc;
+
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Random;
+
+public class ProverbsModel extends Observable {
+
+    private ArrayList<String> proverbs;
+    private String currentProverb;
+    private Random rand;
+
+    public ProverbsModel(){
+        currentProverb = "Set a jemsus proverb ma'am";
+
+        proverbs = new ArrayList<>();
+        proverbs.add("SENTO.....LA FOVZA");
+        proverbs.add("Spero che stiate bene e che siate a casa");
+        proverbs.add("Quando siete di fronte ad un problema che " +
+                "non riuscite a risolvere, il modo migliore di affrontarlo " +
+                "è....diciamo....trovare la soluzione.");
+        proverbs.add("Microsoft sticazzi!");
+        proverbs.add("Attento, stai già pensando alla soluzione del problema.");
+
+        rand = new Random();
+    }
+
+    public String getCurrentProverb() {
+        return currentProverb;
+    }
+
+    public void setCurrentProverb() {
+        int index = rand.nextInt(proverbs.size());
+        currentProverb = proverbs.get(index);
+
+
+    }
+}
