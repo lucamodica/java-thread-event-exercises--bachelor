20fe122ae6f8586504f165583d825db2c0cd23c1
Luca Modica <luca.modica@edu.unito.it>
2021-11-12 12:52:17 +0100

    Added partial thread exercises

---
+++ 08_thread/src/com/demo/App.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import java.util.List;
+
+public class App {
+
+    class Step extends Thread{
+        List<Step> precondition;
+        public Step(String){
+
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+    }
+}
